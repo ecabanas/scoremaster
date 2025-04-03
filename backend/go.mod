c01df6c354373318eaa4ad066091af1192eb0dcf
Eric <93eric@gmail.com>
2025-04-03 16:09:49 +0200

    added field validators

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module scoremaster/backend
 go 1.24.1
 
 require (
+	github.com/go-playground/validator/v10 v10.26.0
 	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
@@ -14,9 +15,16 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.9.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.27 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 )
--- backend/models/participant-model.go
+++ backend/models/participant-model.go
@@ -3,14 +3,23 @@ package models
 import (
 	"time"
 
+	"github.com/go-playground/validator/v10"
+
 	"gorm.io/gorm"
 )
 
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
+
 type Participant struct {
 	gorm.Model
-	Email          string    `json:"email" gorm:"unique"`
-	SubmissionTime time.Time `json:"submission_time"`
-	Metadata       string    `json:"metadata" gorm:"type:json"`
+	Email              string              `json:"email" gorm:"unique"`
+	SubmissionTime     time.Time           `json:"submission_time"`
+	ParticipantAnswers []ParticipantAnswer `json:"participant_answers" gorm:"foreignKey:ParticipantID"`
+	Metadata           string              `json:"metadata" gorm:"type:json"`
 }
 
 type ParticipantAnswer struct {
@@ -20,4 +29,15 @@ type ParticipantAnswer struct {
 	SelectedAnswerID uint      `json:"selected_answer_id"`
 	Score            int       `json:"score"`
 	AnswerTimestamp  time.Time `json:"answer_timestamp"`
+}
+
+func (p *Participant) Validate() error {
+	return validate.Struct(p)
+}
+func (p *Participant) BeforeCreate(tx *gorm.DB) error {
+	return p.Validate()
+}
+
+func (p *Participant) BeforeUpdate(tx *gorm.DB) error {
+	return p.Validate()
 }
